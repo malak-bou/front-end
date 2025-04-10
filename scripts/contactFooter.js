0a08a7f15c0ffa3241fdf677bf276f74a6980cf4
Malak <malakbouabache19@gmail.com>
2025-04-10 13:27:15 +0200

    4th

--- scripts/contactFooter.js
+++ scripts/contactFooter.js
@@ -1,26 +1,6 @@
-// const path = window.location.pathname;
-
-// if (path.endsWith("index.html") || path === "/" || path === "/index.html") {
-//   // You're on the homepage
-//   fetch("pages/contactFooter.html")
-//     .then(res => res.text())
-//     .then(data => {
-//       document.getElementById("contact-container2").innerHTML = data;
-//     });
-// } else {
-//   // You're on a page inside /pages/
-//   fetch("contactFooter.html")
-//     .then(res => res.text())
-//     .then(data => {
-//       document.getElementById("contact-container2").innerHTML = data;
-//     });
-// }
 const path = window.location.pathname;
 
-// Grab just the last part of the path
-const lastSegment = path.split('/').pop();
-
-if (lastSegment === "" || lastSegment === "index.html") {
+if (path.endsWith("index.html") || path === "/" || path === "/index.html") {
   // You're on the homepage
   fetch("pages/contactFooter.html")
     .then(res => res.text())
@@ -28,7 +8,7 @@ if (lastSegment === "" || lastSegment === "index.html") {
       document.getElementById("contact-container2").innerHTML = data;
     });
 } else {
-  // You're inside /pages/
+  // You're on a page inside /pages/
   fetch("contactFooter.html")
     .then(res => res.text())
     .then(data => {
