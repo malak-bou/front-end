d4176b5a44ee43902c4fac50fe7a5f03d635c165
Malak <malakbouabache19@gmail.com>
2025-04-29 02:14:44 +0200

    reglit l RH

--- scripts/RH-aide.js
+++
@@ -1,6 +0,0 @@
-// side barre
-
-    // Fonction pour gérer l'affichage de la barre de navigation
-    function toggleNav() {
-        document.getElementById("sidebar").classList.toggle("active"); // Ajouter ou supprimer la classe active
-    }
